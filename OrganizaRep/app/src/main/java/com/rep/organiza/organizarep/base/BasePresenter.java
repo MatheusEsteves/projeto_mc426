01b23de63b34e7a5d4b7f61d5e3a8af6350866a9
gabriel <ra197460@@students.ic.unicamp.br>
2019-03-12 14:35:15 -0300

    build failure corrected

--- OrganizaRep/app/src/main/java/com/rep/organiza/organizarep/base/BasePresenter.java
+++ OrganizaRep/app/src/main/java/com/rep/organiza/organizarep/base/BasePresenter.java
@@ -3,10 +3,8 @@ package com.rep.organiza.organizarep.base;
 import android.content.Context;
 
 import com.google.gson.JsonObject;
+import com.rep.organiza.organizarep.api.Services;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import br.com.livelo.app.api.Services;
 import retrofit2.Call;
 
 public abstract class BasePresenter {
